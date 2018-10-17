980bcd7794874e0b5f0739f42b77dc1c43745509
zb1006801184 <www.1006801184@qq.com>
2018-10-17 22:11:35 +0800

    z

--- BlockDemo/BlockDemo/ViewController.m
+++ BlockDemo/BlockDemo/ViewController.m
@@ -22,7 +22,6 @@
     TextView *textView = [[TextView alloc]initWithFrame:CGRectMake(0, 0, 100, 100)];
     textView.backgroundColor = [UIColor redColor];
     [self.view addSubview:textView];
-    UILabel *a ;
     //最顶层视图
     TextViewTop *topView = [[TextViewTop alloc]initWithFrame:CGRectMake(0, 0, 50, 50)];
     topView.backgroundColor = [UIColor blueColor];
